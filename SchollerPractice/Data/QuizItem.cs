34292d50ddcdaa6922e9d6d9a20f5d4cb1b7dc01
forgoa <adam.forgo@continental-corporation.com>
2022-05-25 09:42:07 +0200

    updated questions and resources

--- SchollerPractice/Data/QuizItem.cs
+++ SchollerPractice/Data/QuizItem.cs
@@ -20,9 +20,10 @@ namespace SchollerPractice.Data
             public string Question { get; set; }
             public List<string> Choices { get; set; }
             public int AnswerIndex { get; set; }
-            public int Score { get; set; }
-        public string ResultClass { get; set; } = "my-header-h3";
-        public QuestionOrAnswerType QuestionAnswerType { get; set; }
+            public string Answer { get; set; } = "";
+        public int Score { get; set; } = 1;
+            public string ResultClass { get; set; } = "my-header-h3";
+            public QuestionOrAnswerType QuestionAnswerType { get; set; }
             public QuestionOrAnswerType QuestionQuestionType { get; set; }
             public string QuestionImage { get; set; }
             public QuizItem()
